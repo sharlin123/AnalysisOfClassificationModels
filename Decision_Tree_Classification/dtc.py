e7cdf1e7469590a28b4a7f3b16906b8abf9521cf
sharlin123 <sharlin.skr@gmail.com>
2019-07-13 13:17:52 +0530

    ML

---
+++ Decision_Tree_Classification/dtc.py
@@ -0,0 +1,70 @@
+# -*- coding: utf-8 -*-
+#Importing Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+#Importing Dataset
+dataset = pd.read_csv('Social_Network_Ads.csv')
+x = dataset.iloc[:, 2:-1].values
+y = dataset.iloc[:, -1].values
+
+# Splitting into TestSet and Training Set
+from sklearn.model_selection import train_test_split
+xTrain, xTest, yTrain, yTest = train_test_split(x, y, test_size = 0.25, random_state = 0)
+
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+scX = StandardScaler()
+xTrain = scX.fit_transform(xTrain)
+xTest = scX.transform(xTest)
+
+# Fitting Classifier on Training Set
+from sklearn.tree import DecisionTreeClassifier
+classifier = DecisionTreeClassifier(criterion = 'entropy', random_state = 0)
+classifier.fit(xTrain, yTrain)
+
+# Predict Test Set Results
+yPred = classifier.predict(xTest)
+
+# Make Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(yTest, yPred)
+
+# Visualising Training Set Results
+from matplotlib.colors import ListedColormap
+xSet, ySet = xTrain, yTrain
+X1, X2 = np.meshgrid(np.arange(start = xSet[:, 0].min() - 1, stop = xSet[:, 0].max() + 1, step = 0.01), 
+                     np.arange(start = xSet[:, 1].min() - 1, stop = xSet[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.xlim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(ySet)):
+    plt.scatter(xSet[ySet == j, 0], xSet[ySet == j, 1], 
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Training)')
+plt.xlabel('Age')
+plt.ylabel('Salary')
+plt.legend()
+plt.show()
+
+# Visualising Test Set Results    
+xSet, ySet = xTest, yTest
+X1, X2 = np.meshgrid(np.arange(start = xSet[:, 0].min() - 1, stop = xSet[:, 0].max() + 1, step = 0.01), 
+                     np.arange(start = xSet[:, 1].min() - 1, stop = xSet[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.xlim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(ySet)):
+    plt.scatter(xSet[ySet == j, 0], xSet[ySet == j, 1], 
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Decision Tree (Test)')
+plt.xlabel('Age')
+plt.ylabel('Salary')
+plt.legend()
+plt.show()
+    
+
+
